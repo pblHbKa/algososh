9ac44bc3e872054360b92b2494c144d5d757bc9c
Natalya Belikova <minaevanv@inbox.ru>
2023-04-01 12:10:45 +0300

    fibonacci e2e

---
+++ cypress/e2e/fibonacci.cy.js
@@ -0,0 +1,30 @@
+describe("страница c последовательностью Фибоначчи работает правильно", function () {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/fibonacci");
+  });
+
+  it("кнопка запуска недоступна при отсутствии текста", function () {
+    Cypress.config("requestTimeout", 10000);
+    cy.get("input").should("be.empty");
+    cy.get("button").should("be.disabled");
+  });
+
+  it("числа генерируются корректно", function () {
+    const length = 5;
+    let prev1 = 0;
+    let prev2 = 1;
+    cy.get("input").type(length);
+    cy.contains("Сгенерировать").click();
+
+    cy.wait(550 * length);
+
+    cy.get('div[class^="circle_circle"]').then((circles) => {
+      expect(circles[0]).to.have.text(String(prev1));
+      expect(circles[1]).to.have.text(String(prev2));
+      for (let i = 2; i <= length; i++) {
+        expect(circles[i]).to.have.text(String(prev1 + prev2));
+        [prev1, prev2] = [prev2, prev1 + prev2];
+      }
+    });
+  });
+});
